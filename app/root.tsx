0a7fe9b65e2d1ce042183df8b1096c0334dccd61
Mensurui <mensurk3000@gmail.com>
2023-10-25 09:24:25 +0300

    my-remix-app-for-mekina.com

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,17 @@
+import type { LinksFunction } from "@remix-run/node";
 import {
-  Form,
   Links,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css";
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 export default function App() {
   return (
     <html lang="en">
@@ -17,35 +22,9 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+        <div id="homepage">
+        <Outlet/>
         </div>
-
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
---
+++ app/routes/_index.tsx
@@ -0,0 +1,7 @@
+export default function homepage(){
+    return(
+        <h1 className="text-3xl font-bold underline ">
+        Homepage    
+        </h1>
+    )
+}
